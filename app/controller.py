39d4ebc79fa9e2006de938b5e4ebe3bbf801f41c
dorritochan <dorritochan@icloud.com>
2023-02-16 13:35:59 +1300

    Login template done

---
+++ app/controller.py
@@ -0,0 +1,20 @@
+from flask import render_template, flash, redirect, url_for
+from app import app
+from app.forms import LoginForm
+
+@app.route('/')
+@app.route('/home')
+@app.route('/index')
+def index():
+    return render_template('index.html', title='Pottery log')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    # POST method
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    # GET method    
+    return render_template('login.html', title='Login', form=form)
